a5ac091c3c2a05b9d940d677cbf3d60cddd3c7ba
We6geek <tritonover@gmail.com>
2025-11-17 05:20:32 +0300

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Vue-counter/'
+  base: './', // Или '/vue-counter/' если хотите абсолютные пути
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
